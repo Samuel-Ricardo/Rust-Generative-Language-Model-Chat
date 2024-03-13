e883d0060663fbfea958276ad9c51a697850b845
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-13 10:57:11 -0300

    [ :art: ] | update conversation in real time (app::page)

--- rusty-llama/src/app.rs
+++ rusty-llama/src/app.rs
@@ -1,7 +1,10 @@
-use leptos::*;
+use leptos::{html::input, server_fn::response, *};
 use leptos_meta::*;
 
-use crate::model::conversation::{Conversation, Message};
+use crate::{
+    api::converse,
+    model::conversation::{Conversation, Message},
+};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -20,7 +23,25 @@ pub fn App() -> impl IntoView {
 
         set_conversation.update(move |c| c.messages.push(user_message));
 
-        // TODO - converse
+        converse(conversation.get())
+    });
+
+    // React | useEffect
+    create_effect(move |_| {
+        if let Some(_) = send.input().get() {
+            let model_message = Message {
+                text: String::from("..."),
+                user: false,
+            };
+
+            set_conversation.update(move |c| c.messages.push(model_message));
+        };
+    });
+
+    create_effect(move |_| {
+        if let Some(Ok(response)) = send.value().get() {
+            set_conversation.update(move |c| c.messages.last_mut().unwrap().text = response);
+        }
     });
 
     view! {
